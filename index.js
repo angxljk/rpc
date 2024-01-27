ef18a9ebfbf19adab79149c1144d9f7b68ad7d5e
angxljk <153478964+angxljk@users.noreply.github.com>
2024-01-27 16:40:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,47 +3,54 @@ const client = new Discord.Client({
   readyStatus: false,
   checkUpdate: false
 });
+
 const keepAlive = require('./server.js');
 keepAlive();
+
 function formatTime() { //Credits to himika#0001 and never#0001
   const date = new Date();
   const options = {
-    timeZone: 'America/New_York', //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    timeZone: 'America/Port_of_Spain', //https://www.zeitverschiebung.net/en/ and find your city and enter here
     hour12: true,
     hour: 'numeric',
     minute: 'numeric'
   };
   return new Intl.DateTimeFormat('en-US', options).format(date);
 }
+
 client.on('ready', async () => {
   console.clear();
-  console.log(`${client.user.tag} - rich presence started!`);
+  console.log(`${client.user.tag} - rpc started! check your profile ^^`);
+
   const r = new Discord.RichPresence()
     .setApplicationId('1087940913348743189')
     .setType('STREAMING')
-    .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ') //Must be a youtube video link 
-    .setState('Recording')
-    .setName('test')
-    .setDetails(`test [${formatTime()}]`)
+    .setURL('https://youtube.com/watch?v=xvFZjo5PgG0') //Must be a youtube video link 
+    .setState('Sleepy')
+    .setName('randomness')
+    .setDetails(`Hey 👋⚓ [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Boring') //Text when you hover the Large image
-    .setAssetsSmallImage('') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Twitch') //Text when you hover the Small image
-    .addButton('Watch', 'https://twitch.tv/developer')
-    .addButton('Donate', 'https://discord.com/channels/1126406880881815625/1126407676688080967');
+ .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1150242860273254460/1190309175566282843/majo-no-tabitabi-the-journey-of-elaina_2.gif') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('insert funny text') //Text when you hover the Large image
+    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1150242860273254460/1190250393746280509/cc5323ff-e634-4a9b-8320-2cb09efa665a.gif') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('You weirdo, why would you hover over the small image.') //Text when you hover the Small image
+    .addButton('My Site 🔨', 'https://vornexx.vercel.app/')
+    .addButton('In depth bio 💞', 'https://rentry.co/vornexx')
+
   client.user.setActivity(r);
   client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
+
   let prevTime = null;
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = ` [${newTime}]`;
+      const newDetails = `Hey 👋⚓ [${newTime}]`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
     }
   }, 1000); // Update every second
 });
+
 const mySecret = process.env['TOKEN'];
 client.login(mySecret);
