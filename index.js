ac33532a86931c1f2717f44a5cb01188f3e219d4
angxljk <153478964+angxljk@users.noreply.github.com>
2024-02-10 17:00:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ client.on('ready', async () => {
     .setDetails(`𝟏𝟒/𝟎𝟐/𝟐𝟎𝟐𝟒 [${formatTime()}]`)
     .setStartTimestamp(Date.now())
  .setAssetsLargeImage('https://media.discordapp.net/attachments/1188998771359629422/1205972735407427634/71821fa420e0599842fd1d8fda97f889.jpg?ex=65da50a2&is=65c7dba2&hm=4c06e7400206d762a0ba2ea5314b9c0fa11d253e913cba053ad27c5a645c9931&=&format=webp&width=306&height=409') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('<t:1707926400:d') //Text when you hover the Large image
+    .setAssetsLargeText('𝟏𝟒/𝟎𝟐/𝟐𝟎𝟐𝟒') //Text when you hover the Large image
     .setAssetsSmallImage('https://media.discordapp.net/attachments/1188998771359629422/1205972735629721650/daa55441c0c85b74a2602aefb7b1da33.jpg?ex=65da50a2&is=65c7dba2&hm=26fa0c7feacc7748117f4927d3b6b3904e210dea1b66c358decdab5656c4510d&=&format=webp&width=230&height=408') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('gg') //Text when you hover the Small image
+    .setAssetsSmallText('17y') //Text when you hover the Small image
     .addButton('BlackParty♱', 'https://discord.gg/UKpgfW5J')
     .addButton('Legacys♱', 'https://discord.gg/HvsYb4kK');
 
