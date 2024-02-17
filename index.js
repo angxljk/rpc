d0d945bab9a96650c7b9ca607cde2a584ce09fb9
angxljk <153478964+angxljk@users.noreply.github.com>
2024-02-16 22:49:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -32,8 +32,7 @@ client.on('ready', async () => {
     .setStartTimestamp(Date.now())
  .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1188998771359629422/1208184673495425144/e9bcb2739609c302effaf0d8801da355.png?ex=65e25ca9&is=65cfe7a9&hm=a860b20895f0140f967d91c37827050a42d33340f514ab2fa7f7248f40ff4b30&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('𝟏𝟒/𝟎𝟐/𝟐𝟎𝟐𝟒') //Text when you hover the Large image
-    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1188998771359629422/1208186096362721300/8065c9e0231029e8fda40e7eaeb0d933.png?ex=65e25dfc&is=65cfe8fc&hm=e213209240db975d4fe64438d36646dfaa22423a43eea520ae469eef9d12d434&
-                         ') //You can put links in tenor or discord and etc.
+    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1188998771359629422/1208186096362721300/8065c9e0231029e8fda40e7eaeb0d933.png?ex=65e25dfc&is=65cfe8fc&hm=e213209240db975d4fe64438d36646dfaa22423a43eea520ae469eef9d12d434&') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('17y') //Text when you hover the Small image
     .addButton('BlackParty♱', 'https://discord.gg/kMzbwgDwcP')
     .addButton('Legacys♱', 'https://discord.gg/HvsYb4kK');
